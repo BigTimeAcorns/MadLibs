d23cd081ec73da3d95b452e8734a2f61e8b7288d
Dannels <caded12373@stu.powayusd.com>
2022-09-21 14:41:17 -0700

    started

--- MadLibs.java
+++ MadLibs.java
@@ -1,3 +1,6 @@
 public class MadLibs{
-    
+    public static void main(String[] args) {
+        String story = "I went to the animal <noun>, the <plural_noun> and the <number> beasts were there.";
+        System.out.println(story.indexOf("<"));
+    }
 }
