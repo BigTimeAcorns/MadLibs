7403082db512a18e79c9d6fc70f3496d92a00375
Dannels <caded12373@stu.powayusd.com>
2022-09-22 14:10:07 -0700

    Final Code

--- MadLibs.java
+++ MadLibs.java
@@ -1,6 +1,42 @@
+/*
+Madlib story
+Cade Dannels,  Jake Gyberg
+AP Computer Science A
+9/22/2022
+A Madlib program that can be used for any story outline
+*/ 
+
+
+import java.util.Scanner;
 public class MadLibs{
     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        //Sory template
         String story = "I went to the animal <noun>, the <plural_noun> and the <number> beasts were there.";
-        System.out.println(story.indexOf("<"));
+
+        //Runs until there are no more blank spots
+        while (story.contains("<")) {
+            //find next blank
+            int index1 = story.indexOf("<");
+            int index2 = story.indexOf(">");
+
+            //Prompt for user input
+            System.out.println("Enter a(n) "+story.substring(index1+1, index2));
+
+            //Gets user input and puts it in variable
+            String answer = sc.nextLine();
+
+            //Changes story string
+            story = story.replaceFirst("<" + story.substring(index1+1, index2)+ ">", answer);
+
+        }
+        
+        
+        //Print completed story
+        System.out.println(story);
+        
+        sc.close();
     }
 }
